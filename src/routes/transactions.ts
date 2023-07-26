12f7d14fe2bfff1564ad889666ad9f4566c6b196
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-26 16:56:43 -0300

    feat: cookies configured and middleware created

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,36 +2,72 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 // obs: os plugins do fastify precisam ser asyncronos sempre
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
-    // retornando um objeto pois se em algum
-    // momento precisar de mais informações fica
-    // mais facil de buscar
-    return { transactions }
-  })
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-  app.get('/:id', async (request) => {
-    // criando um schema (tipo) para o request.params
-    // que vem da requisição
-    const getTransactionParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
-    const { id } = getTransactionParamsSchema.parse(request.params)
-    // o first() garante que só vai retornar um único elemento
-    const transaction = await knex('transactions').where('id', id).first()
-    return { transaction }
-  })
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
+      // retornando um objeto pois se em algum
+      // momento precisar de mais informações fica
+      // mais facil de buscar
+      return { transactions }
+    }
+  )
 
-  app.get('/summary', async () => {
-    // rota de resumo
-    // faz uma busca na collectio de transactions e soma todos os amounts
-    // do usuário especifico, o resultado insere em um campo único amount 
-    const summary = await knex('transactions').sum('amount', { as: 'amount'}).first()
-    return { summary }
-  })
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      // criando um schema (tipo) para o request.params
+      // que vem da requisição
+      const getTransactionParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+      const { id } = getTransactionParamsSchema.parse(request.params)
+
+      const { sessionId } = request.cookies
+
+      // o first() garante que só vai retornar um único elemento
+      const transaction = await knex('transactions')
+        .where({
+          session_id: sessionId,
+          id,
+        })
+        .first()
+      return { transaction }
+    }
+  )
+
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
+
+      // rota de resumo
+      // faz uma busca na collectio de transactions e soma todos os amounts
+      // do usuário especifico, o resultado insere em um campo único amount
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
+      return { summary }
+    }
+  )
 
   app.post('/', async (request, reply) => {
     // usando o zod para tipar (criando um schema) os dados
@@ -48,13 +84,31 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     )
 
+    // verifico se ja existe o sessionId
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      // se não existir crio um passando o randomUUID()
+      sessionId = randomUUID()
+
+      // salvo nos coockies uma informação chamada
+      // 'sessionId' com o valor sessionId
+      // indico as rotas do backend que vão ter acesso ao cookie
+      // informo quanto tempo em (milissegundos) esse cookie vai ser válido
+      // no navegador do usuário. Obs: depois desse período ele expira
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
+      })
+    }
+
     // agora vou inserir no banco de dados
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
-
     return reply.status(201).send()
   })
 }
