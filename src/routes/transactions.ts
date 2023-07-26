f1d80ada71130324f8ab3d7aeab15b44bf67d24c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-26 12:26:26 -0300

    feat: create findAll and findById transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,26 @@ import { knex } from '../database'
 
 // obs: os plugins do fastify precisam ser asyncronos sempre
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    // retornando um objeto pois se em algum
+    // momento precisar de mais informações fica
+    // mais facil de buscar
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    // criando um schema (tipo) para o request.params
+    // que vem da requisição
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+    // o first() garante que só vai retornar um único elemento
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     // usando o zod para tipar (criando um schema) os dados
     const createTransactionBodySchema = z.object({
