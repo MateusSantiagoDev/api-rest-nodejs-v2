697e1e0ac5a9e5b14ebde1e8ab0a933f1ef3f688
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-26 20:50:22 -0300

    feat: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -20,10 +20,13 @@ if (process.env.NODE_ENV === 'test') {
 // o enum esta sendo usado para passar as opções e o default é em caso
 // de não ser informado o ambiente
 
+// obs: linha 27 => pg para o knex entender que é postgress (deploy)
+
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
